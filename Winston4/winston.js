b420ec4055db25caf629d7019a64dc217be66bfa
dear.raed <med.raed.besbes@takiacademyteam.com>
2022-10-27 09:50:41 +0100

    wisnton with middleware

--- Winston4/winston.js
+++ Winston4/winston.js
@@ -19,12 +19,38 @@ const handler = (func) => (req, res) => {
         res.send('Oh no, something did not go well!');
     }
 };
-
+//Log As an independent Middleware
 app.use( (req, res, done) => {
     logger.info(req.originalUrl);
     done();
 });
 
+  function errorHandler  (error, request, response, next) {
+    logger.info("catched error");
+    // Error handling middleware functionality
+    console.log( `error ${error.message}`) // log the error
+    const status = error.status || 400
+    // send back an easily understandable error message to the caller
+    response.status(status).send({status: "fail", error: error.message})
+  }
+
+
+
+//Examples with a function Middleware
 app.get('/success', handler((req, res) => { res.send('Yay!'); }))
-app.get('/error', handler((req, res) => { throw new Error('Doh!'); }))
+//Example Witj a middleware Log
+app.get('/error', (req, res) => { throw new Error('Doh!'); });
+app.get('/testErrorMiddleware', handler((req, res) => {four(4).then(function(x) {res.send({"yes : ": x})}).catch(function(x){res.send({"no : ": x})}) }));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.use(errorHandler);
+
+function four (x) {
+    return new Promise((resolve, reject) => {
+      if (x > 5) {
+        resolve(x);
+      } else {
+        reject(x);
+      }
+    });
+  }
